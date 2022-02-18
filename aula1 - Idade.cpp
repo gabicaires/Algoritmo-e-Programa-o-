28df95b8f728f9bf44d7aa327b4eaf47b411971d
gabicaires <cairessoares59@gmail.com>
2022-02-18 10:44:32 -0300

    Primeiro Projeto

---
+++ aula1 - Idade.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h> //biblioteca
+
+int main(){//inicio do algoritimo
+ int idade;  //inteiro idade;
+printf("digite sua idade: "); //escreva ("digite sua idade") 
+scanf("%d", &idade); //leia (idade)
+
+printf("A sua idade é: %d", idade);
+
+}
